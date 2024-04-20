d8e0affc2953fdd3ff956142787b7241d5e066e2
tristanff <tristan.flechard@hotmail.fr>
2024-04-20 18:18:31 -0300

    Changed Server Port Again

--- Back-End/server.py
+++ Back-End/server.py
@@ -75,4 +75,4 @@ def analysis_route():
 
 # Start the Flask server
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)  # Change the port if needed
+    app.run(debug=True, host='0.0.0.0', port=5000)  # Change the port if needed
