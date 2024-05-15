f6dc7b89010e642bac5b00b9c1b724a188e7cab5
tristanff <tristan.flechard@hotmail.fr>
2024-05-15 19:14:38 -0300

    second test

--- Compute/process.py
+++ Compute/process.py
@@ -3,7 +3,7 @@ import requests
 from pysentimiento import create_analyzer
 
 # Create sentiment analyzer
-#analyzer = create_analyzer(task="sentiment", lang="en")
+analyzer = create_analyzer(task="sentiment", lang="en")
 
 api_url = os.getenv("API_URL")
 print(f"api_url: {api_url}\n")
