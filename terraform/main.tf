a381cceaaf266360d393d7d70c2ee79eccee01b8
Reversive <mpavan@itba.edu.ar@>
2024-05-29 02:46:05 -0300

    feat: lower and uppercase force for names and types

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,16 @@
 module "network" {
   source      = "./modules/network"
-  vpc_name    = var.vpc_name
+  vpc_name    = lower(var.vpc_name)
   vpc_cidr    = var.vpc_cidr
   public_cidr = var.public_cidr
-  public_az   = var.public_az
-  
+  public_az   = lower(var.public_az)
+
 }
 
 module "database" {
   source          = "./modules/database"
   table_names     = var.table_names
-  db_billing_mode = var.db_billing_mode
+  db_billing_mode = upper(var.db_billing_mode)
 }
 
 module "security" {
