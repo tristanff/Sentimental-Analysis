85ca053d150ce32916f89a535d51f7e71e9b37a9
Reversive <matiaspavan@gmail.com>
2024-07-17 14:15:15 -0300

    feat: added mail signup and confirmation

--- terraform/modules/application/main.tf
+++ terraform/modules/application/main.tf
@@ -1,21 +1,51 @@
 resource "aws_cognito_user_pool" "user_pool" {
   name = "sa-pool"
-}
+  username_attributes = ["email"]
+  auto_verified_attributes = ["email"]
+  password_policy {
+    minimum_length = 6
+  }
 
-resource "aws_cognito_user_pool_domain" "cognito-domain" {
-  domain       = "sentimental-analysis"
-  user_pool_id = aws_cognito_user_pool.user_pool.id
+  verification_message_template {
+    default_email_option = "CONFIRM_WITH_CODE"
+    email_subject = "Account Confirmation"
+    email_message = "Your confirmation code is {####}"
+  }
+
+  schema {
+    attribute_data_type      = "String"
+    developer_only_attribute = false
+    mutable                  = true
+    name                     = "email"
+    required                 = true
+
+    string_attribute_constraints {
+      min_length = 1
+      max_length = 256
+    }
+  }
 }
 
 resource "aws_cognito_user_pool_client" "userpool_client" {
   name                                 = "client"
   user_pool_id                         = aws_cognito_user_pool.user_pool.id
-  callback_urls                        = ["https://localhost.com"]
-  allowed_oauth_flows_user_pool_client = true
+  callback_urls                        = ["https://localhost"]
   allowed_oauth_flows                  = ["implicit"]
+  allowed_oauth_flows_user_pool_client = true
   allowed_oauth_scopes                 = ["email", "openid"]
   supported_identity_providers         = ["COGNITO"]
+  explicit_auth_flows = [
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_ADMIN_USER_PASSWORD_AUTH"
+  ]
 }
+
+resource "aws_cognito_user_pool_domain" "cognito-domain" {
+  domain       = "sentimental-analysis2"
+  user_pool_id = "${aws_cognito_user_pool.user_pool.id}" #aws_cognito_user_pool.user_pool.id
+}
+
 
 # tweets_raw lambda
 data "archive_file" "tweets_raw" {
